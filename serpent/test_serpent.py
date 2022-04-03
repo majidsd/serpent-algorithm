5c525e63db804ee294111f9ad270c4b84641a7a7
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2022-04-03 23:36:07 +0200

    Refine serpent test class, remove unused code and refine code structure

--- serpent/test_serpent.py
+++ serpent/test_serpent.py
@@ -8,34 +8,29 @@ import observer as observer
 
 def main():
     observer_object = observer.Observer(["plainText", "userKey", "cipherText"])
-    optList, rest = getopt.getopt(sys.argv[1:], "edhbt:k:p:c:i:")
+    opts, args = getopt.getopt(sys.argv[1:], "edhbt:k:p:c:i:")
     
-    if rest:
-        help_functions.helpExit("Sorry, can't make sense of this: '%s'" % rest)
+    if args:
+        help_functions.helpExit("Sorry, can't make sense of this: '%s'" % args)
 
-    # Transform the list of options into a more comfortable
-    # dictionary. This only works with non-repeated options, though, so
-    # tags (which are repeated) must be dealt with separately.
     options = {}
-    
-    for key, value in optList:
-        if key == "-t":
-            observer_object.addTag(value)
+    for opt, arg in opts:
+        if opt == "-t":
+            observer_object.addTag(arg)
         else:
-            if key in options.keys():
-                help_functions.helpExit("Multiple occurrences of " + key)
+            if opt in options.keys():
+                help_functions.helpExit("Multiple occurrences of " + opt)
             else:
-                options[str.strip(key)] = str.strip(value)
+                options[str.strip(opt)] = str.strip(arg)
     
     # Not more than one mode
     mode = None
-    
-    for k in options.keys():
-        if k in ["-e", "-d", "-h"]:
+    for key in options.keys():
+        if key in ["-e", "-d", "-h"]:
             if mode:
-                help_functions.helpExit("you can only specify one mode")
+                help_functions.helpExit("You can only specify one mode")
             else:
-                mode = k
+                mode = key
 
     if not mode:
         help_functions.helpExit("No mode specified")
@@ -49,38 +44,27 @@ def main():
     if ('-c') in str(options):
         cipherText = options["-c"]
         
-
     if mode == "-e":
         if not plainText:
             help_functions.helpExit("-p (plaintext) is required when doing -e (encrypt)")
     if mode == "-d":
         if not cipherText:
             help_functions.helpExit("-c (ciphertext) is required when doing -d (decrypt)")
-            
-    
-    # Perform the action specified by the mode
-    # NOTE that the observer will automatically print the basic stuff such
-    # as plainText, userKey and cipherText (in the right format too), so we
-    # only need to perform the action, without adding any extra print
-    # statements here.
+             
     if mode == "-e":
         userKey = help_functions.key_gen()
-        userKey = options["-k"]
-        print(userKey)
-        print('Plain text=', plainText)
-        print("The Cipher text is: ", normal_serpent.encrypt(plainText, userKey))
+        print('************************** Starting encryption **************************')
+        print('The Plain text is: ', plainText)
+        print("The Cipher text is: ", normal_serpent.encrypt(plainText, help_functions.convertToBitstring(userKey, 256)))
         print("The key is: ", userKey)
-
     elif mode == "-d":
         userKey = options["-k"]
-        # iv = options["-i"]
-
         if not userKey:
-            help_functions.helpExit("-k (key) required with -d (decrypt)")
-                
-        print('Cipher text: ', cipherText)
-        print("The Plain text is: ", normal_serpent.decrypt(cipherText, userKey))
-
+            help_functions.helpExit('-k (key) required with -d (decrypt)')
+     
+        print('************************** Starting decryption **************************')
+        print('The Cipher text is: ', cipherText)
+        print('The Plain text is: ', normal_serpent.decrypt(cipherText, help_functions.convertToBitstring(userKey, 256)))
     else:
         help_functions.helpExit()
 
